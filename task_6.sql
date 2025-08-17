a9306cb9ee52ce5d9c9438e90c2758c679293e6a
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-08-17 22:30:34 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,12 @@
 -- task_6.sql
--- This script inserts multiple rows into the 'customer' table of the 'alx_book_store' database.
--- The database name is expected to be passed as an argument to the mysql command.
+-- This script inserts multiple rows into the 'customer' table 
+-- of the 'alx_book_store' database.
+-- The database name is expected to be passed as an argument 
+-- to the mysql command.
 
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+USE alx_book_store;
+
+INSERT INTO customer (customer_name, email, address) VALUES
+    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    ('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
