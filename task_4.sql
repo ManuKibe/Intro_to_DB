79667bc0a3b5ee7c396519cea5a2448292fbf768
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-08-17 22:39:49 +0300

    Delete task_4.sql

--- task_4.sql
+++
@@ -1,6 +0,0 @@
--- task_4.sql
--- This script prints the full description of the table 'books' from the database 'alx_book_store'.
--- The database name is expected to be passed as an argument to the mysql command.
--- You are not allowed to use DESCRIBE or EXPLAIN statements.
-
-SHOW CREATE TABLE books;
