978000cd3ca9560812ce97841126d99e5aca6f50
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-08-17 22:22:36 +0300

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- task_5.sql
+-- This script inserts a single row into the 'customer' table of the 'alx_book_store' database.
+-- The database name is expected to be passed as an argument to the mysql command.
+
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
