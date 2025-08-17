42beda4bfbe8b7261f0582bddc075d0bbb4ef612
ManuKibe <131656029+ManuKibe@users.noreply.github.com>
2025-08-17 22:23:52 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,6 @@
 -- task_4.sql
-USE alx_book_store;
+-- This script prints the full description of the table 'books' from the database 'alx_book_store'.
+-- The database name is expected to be passed as an argument to the mysql command.
+-- You are not allowed to use DESCRIBE or EXPLAIN statements.
 
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+SHOW CREATE TABLE books;
